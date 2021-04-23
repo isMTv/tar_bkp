f92e92e25dec833d81217bd96f6d1e9521a37aa7
MTv <viktorspeed@yandex.ru>
2021-04-23 18:00:37 +0300

    Update tar_bkp.sh

--- tar_bkp.sh
+++ tar_bkp.sh
@@ -56,19 +56,19 @@ create_db_bkp () {
     while [ "$opt_m" = "true" ]; do
         while [[ -z "$db_user" || -z "$db_pass" || -z "$db_name" ]]; do echo " - [@create_db_bkp] Error, requared option's "-u, -j, -i"" ; exit 1 ; done
         cd "${source}" || exit 1
-        local name_bkp="${project}-${curdate}.sql.gz.md5"
+        local name_bkp="${project}-${curdate}.sql.gz.sha1"
         db_bkp="${source}/db_bkp" ; if [ ! -e "$db_bkp" ]; then mkdir -p "$db_bkp" ; chmod u=rw,go= "$db_bkp" ; fi
         db_hash="$(mysqldump -u "${db_user}" -p"${db_pass}" "${db_name}" | gzip > /dev/null | sha1sum | awk '{print $1}')"
-        bkp_hash="$(cat "${db_bkp}"/"${name_bkp}" 2> /dev/null)"
+        bkp_hash="$(cat "${db_bkp}"/"${project}"-*.sql.gz.sha1 2> /dev/null)"
         if [ "$db_hash" != "$bkp_hash" ]; then
-            rm -f "${db_bkp}"/"${project}"-*.sql.gz
-            mysqldump -u "${db_user}" -p"${db_pass}" "${db_name}" | gzip > "${db_bkp}"/"${name_bkp::-4}" | sha1sum | awk '{print $1}' > "${db_bkp}"/"${name_bkp}" || exit 1 ; status_cdb="$?"
-            local size_bkp="$(du -sh "${db_bkp}"/"${name_bkp::-4}" | awk '{print $1}')"
-            if [ "$status_cdb" = "0" ] ; then logger "[+] [@create_db_bkp] Резервная копия (${name_bkp::-4}) создана [$size_bkp]." ; fi
+            rm -f "${db_bkp}"/"${project}"-*.sql.gz*
+            mysqldump -u "${db_user}" -p"${db_pass}" "${db_name}" | gzip > "${db_bkp}"/"${name_bkp::-5}" | sha1sum | awk '{print $1}' > "${db_bkp}"/"${name_bkp}" || exit 1 ; status_cdb="$?"
+            local size_bkp="$(du -sh "${db_bkp}"/"${name_bkp::-5}" | awk '{print $1}')"
+            if [ "$status_cdb" = "0" ] ; then logger "[+] [@create_db_bkp] Резервная копия (${name_bkp::-5}) создана [$size_bkp]." ; fi
             break
         else
             local size_bkp="$(du -sh "${db_bkp}"/"${project}"-*.sql.gz | awk '{print $1}')"
-            logger "[-] [@create_db_bkp] В резервной копии (${name_bkp::-4}) изменений не обнаружено [$size_bkp]."
+            logger "[-] [@create_db_bkp] В резервной копии (${name_bkp::-5}) изменений не обнаружено [$size_bkp]."
             break
         fi
     done
